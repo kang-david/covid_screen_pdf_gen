aee6022a043a321a6c2f6d18f1470d9324a5cebd
unknown <12dkang@gmail.com>
2021-12-29 20:00:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A function that generates a Covid-19 screening PDF using Python's FPDF2 library.
+
+Once dependencies are installed, pass your input file paths as parameters inside "create_pdf()" and run the function. This will generate an "output.pdf" file in the root directory.
---
+++ main.py
@@ -0,0 +1,67 @@
+from fpdf import FPDF
+from PIL import Image
+
+def create_pdf(user_logo_file, qr_code_file):
+
+    # Create FPDF object
+    pdf = FPDF('P', 'mm', 'Letter')
+
+    # Add a page
+    pdf.add_page()
+
+    # Set font
+    pdf.set_font('helvetica', 'B', 28)
+
+    # Add page border
+    pdf.set_line_width(2)
+    pdf.line(x1=10, y1=10, x2=pdf.w-10, y2=10)
+    pdf.line(x1=pdf.w-10, y1=10, x2=pdf.w-10, y2=pdf.h-10)
+    pdf.line(x1=pdf.w-10, y1=pdf.h-10, x2=10, y2=pdf.h-10)
+    pdf.line(x1=10, y1=pdf.h-10, x2=10, y2=10)
+
+    # Add user-uploaded logo
+    pixel_to_mm = 0.79375 / 3
+    user_logo = Image.open(user_logo_file)
+    user_logo = user_logo.resize((round(200*(user_logo.width/user_logo.height)),200))
+    pdf.image(
+        user_logo,
+        x=(pdf.w/2)-(user_logo.width*pixel_to_mm/2),
+        y=20,
+        h=200*pixel_to_mm
+    )
+
+    # Add QR code
+    qr_code = Image.open(qr_code_file)
+    pdf.image(
+        qr_code,
+        x=(pdf.w/2)-40,
+        y=80, w=80,
+        h=80
+    )
+
+    # Add text
+    text_lines = [
+        'SCAN THE CODE',
+        'PROVIDE CONTACT INFO',
+        'ENTER & STAY SAFE',
+    ]
+    pdf.multi_cell(0, 160, '', align='C', ln=True)
+    for line in text_lines:
+        pdf.multi_cell(0, 15, line, align='C', ln=1)
+
+    # Add Canatrace logo
+    canatrace_logo = Image.open('images/canatracelogo.png')
+    canatrace_logo = canatrace_logo.resize((401, 92))
+    pdf.image(
+        canatrace_logo,
+        x=(pdf.w/2)-(canatrace_logo.width*pixel_to_mm/2),
+        y=230,
+        h=canatrace_logo.height*pixel_to_mm
+    )
+
+    # Create output file
+    pdf.output('output.pdf')
+
+
+
+create_pdf('images/userlogo.png', 'images/qrcode.png')
